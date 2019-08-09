f604b90e3cfa3c5f7bd849feaffc36a55ad42271
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-08-10 00:16:15 +0200

    refactor: checking if elements collide

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -13,19 +13,9 @@ export const cursorDistance = (first: Pos, second: Pos) => {
     Math.pow(first.left - second.left, 2));
 }
 
-const checkRects = (rects: ClientRect, fileRects: ClientRect, horizontal = true) => {
-  const sideA = horizontal ? 'left' : 'top';
-  const sideB = horizontal ? 'right' : 'bottom';
-
-  return rects[sideA] < fileRects[sideA] && rects[sideB] > fileRects[sideB] ||
-    rects[sideA] > fileRects[sideA] && rects[sideB] < fileRects[sideB] ||
-    rects[sideA] < fileRects[sideA] && fileRects[sideA] < rects[sideB] ||
-    fileRects[sideB] > rects[sideA] && fileRects[sideB] < rects[sideB];
-}
-
 export const elementsCollide = (first: HTMLElement, second: HTMLElement) => {
-  const firstRect = first.getBoundingClientRect();
-  const secondRect = second.getBoundingClientRect();
+  const rect1 = first.getBoundingClientRect();
+  const rect2 = second.getBoundingClientRect();
 
-  return checkRects(firstRect, secondRect) && checkRects(firstRect, secondRect, false);
+  return !(rect1.right < rect2.left || rect1.left > rect2.right || rect1.bottom < rect2.top || rect1.top > rect2.bottom);
 }
