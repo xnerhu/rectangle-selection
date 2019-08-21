31be03ac7d7838cfd74e1d9a63a792cf6d997dd3
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-08-22 00:41:23 +0200

    feat: use key instead of itemKey

--- src/components/SelectableItem/index.tsx
+++ src/components/SelectableItem/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { findDOMNode } from 'react-dom';
 
 interface Props {
-  itemKey: any;
+  itemKey?: any;
 }
 
 interface State {
@@ -13,17 +13,17 @@ export function selectableItem<T>(Wrapped: React.ComponentType<T>): React.Compon
   return class extends React.PureComponent<T & Props, State> {
     componentDidMount() {
       const ref = findDOMNode(this);
-      const { registerItem, itemKey } = this.props as any;
+      const { registerItem, itemKey, key } = this.props as any;
 
       registerItem({
         ref: ref as HTMLElement,
-        value: itemKey,
+        value: itemKey || key,
       });
     }
 
     componentWillUnmount() {
       const { itemKey, unregisterItem } = this.props as any;
-  
+
       unregisterItem(itemKey);
     }
 
